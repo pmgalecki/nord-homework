a69dd1450d046bf380c9c2babbd0d0b7b40987a3
Paweł Galecki <pmgalecki@gmail.com>
2025-03-20 21:52:12 +0100

    ie11 compatibility attempt

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,14 @@
+import 'react-app-polyfill/ie11';
+
 import { createRoot } from 'react-dom/client';
+
+import 'regenerator-runtime/runtime';
+import 'core-js/features/array/find';
+import 'core-js/features/array/includes';
+import 'core-js/features/number/is-nan';
+import 'core-js/features/promise';
+import 'core-js/features/object/assign';
+import 'core-js/features/symbol';
 
 import App from './app';
 
--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,32 @@
 
 import { defineConfig, UserConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import legacy from '@vitejs/plugin-legacy';
+import babel from 'vite-plugin-babel';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    legacy({
+      targets: ['defaults'],
+      additionalLegacyPolyfills: [
+        'regenerator-runtime/runtime',
+        'core-js/features/array/find',
+        'core-js/features/array/includes',
+        'core-js/features/number/is-nan',
+        'core-js/features/promise',
+        'core-js/features/object/assign',
+        'core-js/features/symbol',
+      ],
+    }),
+    babel({
+      babelConfig: {
+        presets: ['@babel/preset-env'],
+        targets: { ie: '11' },
+      },
+    }),
+  ],
   test: {
     environment: 'jsdom',
     globals: true,
